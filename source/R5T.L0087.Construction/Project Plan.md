fe8d7f76aa3a2a2b8133b7352a806d02e57ab986
davidcoats <david@getsafetycone.com>
2024-01-25 10:01:33 -0800

    Initial commit.

---
+++ source/R5T.L0087.Construction/Project Plan.md
@@ -0,0 +1,2 @@
+# R5T.L0087.Construction
+Construction console project for the R5T.L0087 library.
---
+++ source/R5T.L0087/Code/Documentation.cs
@@ -0,0 +1,12 @@
+using System;
+
+
+namespace R5T.L0087
+{
+	/// <summary>
+	/// Testing infrastructure library.
+	/// </summary>
+	public static class Documentation
+	{
+	}
+}
